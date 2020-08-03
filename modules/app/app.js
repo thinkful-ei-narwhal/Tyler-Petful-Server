73d1a367c0987c650e7234516b92058de314aa46
Tyler Sharp <johntylersharp@gmail.com>
2020-08-03 01:15:18 -0400

    cors

--- modules/app/app.js
+++ modules/app/app.js
@@ -9,36 +9,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: CLIENT_ORIGIN,
+    origin: "https://petful-client-xi.vercel.app/",
   })
 );
 
 app.use("/api/people", peopleRouter);
 app.use("/api/pets", petsRouter);
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use((error, req, res, next) => {
   let response;
